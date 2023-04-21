87f52a8e2c0d4e400637755589bd8236d61ec4c2
Younganswer <dudtj1999@gmail.com>
2023-04-21 21:07:03 +0900

    [feat] Config.cpp Server.cpp #11
    Config.cpp, Server.cpp 구현 부 작성

--- srcs/config/Config.cpp
+++ srcs/config/Config.cpp
@@ -4,29 +4,81 @@ Config::Config(const char *file_name): _file_name(file_name) {}
 Config::~Config(void) {}
 
 // Util
-//bool	Config::setServer(Server &server, File_pointer fp) const {
-//	return (true);
-//}
+bool	Config::setServer(Server &server, std::ifstream &infile) const throw(std::exception){
+	std::string	token;
 
-//bool	Config::setServers(std::vector<Server> &servers) throw(std::exception) {
-	//File_pointer fp;
+	infile >> token;
+	if (token != "{") {
+		throw (Config::InvalidSyntaxException());
+	}
 
-	//while (fp != eof()) {
-	//	Server	server;
+	while (infile >> token) {
+		if (token == "listen") {
+			infile >> token;
+			if (token == "80;") {
+				server.setPort(80);
+			} else if (token == "443;") {
+				server.setPort(443);
+			} else {
+				throw (Config::InvalidSyntaxException());
+			}
+		} else if (token == "server_name") {
+			while (infile >> token && token.back() != ';') {
+				server.pushName(token);
+			}
+			server.pushName(token.substr(0, token.size()-1));
+		} else if (token == "client_max_body_size") {
+			infile >> token;
+			server.setClientMaxBodySize(std::stoi(token.substr(0, token.size()-1)));
+			//TODO: calculate unit size
+		} else if (token == "root") {
+			infile >> token;
+			server.setRoot(token.substr(0, token.size()-1));
+		} else if (token == "index") {
+			while (infile >> token && token.back() != ';') {
+				server.pushIndex(token);
+			}
+			server.pushIndex(token.substr(0, token.size()-1));
+		} else if (token == "cgi_pass") {
+			infile >> token;
+			server.setCgiPass(token.substr(0, token.size()-1));
+		} else if (token == "}") {
+			break ;
+		} else {
+			throw (Config::UnknownException());
+		}
+	}
+	return (true);
+}
 
-	//	try {
-	//		setServer(server, fp);
-	//	} catch (Config::ParsingException &e) {
-	//		throw (e);
-	//	} catch (...) {
-	//		throw (Config::UnknownException());
-	//	}
-	//	servers.push_back(server);
-	//}
-	//return (true);
-//}
+bool	Config::setServers(std::vector<Server> &servers) const throw(std::exception) {
+	std::ifstream	infile(this->_file_name);
+	std::string		token;
+
+	if (infile.is_open() == false) {
+		throw (Config::FailToOpenFileException());
+	}
+
+	while (infile >> token) {
+		if (token != "server") {
+			throw (Config::InvalidSyntaxException());
+		}
+
+		Server	server;
+
+		try {
+			this->setServer(server, infile);
+		} catch (Config::InvalidSyntaxException &e) {
+			throw (e);
+		} catch (...) {
+			throw (Config::UnknownException());
+		}
+		servers.push_back(server);
+	}
+	return (true);
+}
 
 // Exception
-const char*	Config::ParsingException::what() const throw() { return ("ParsingException"); }
-
-const char*	Config::UnknownException::what() const throw() { return ("UnknownException"); }
+const char	*Config::FailToOpenFileException::what() const throw() { return ("Fail to open file"); }
+const char	*Config::InvalidSyntaxException::what() const throw() { return ("Invalid syntax"); }
+const char	*Config::UnknownException::what() const throw() { return ("Unknown Exception"); }
---
+++ srcs/server/Server.cpp
@@ -0,0 +1,100 @@
+#include "../../incs/server/Server.hpp"
+
+std::string	Server::_default_err_page = "default_err_page";
+
+Server::Server(void) {}
+Server::~Server(void) {}
+
+// Getters
+const std::string				&Server::getDefaultErrPage(void) { return Server::_default_err_page; }
+int								Server::getPort(void) const { return this->_port; }
+const std::vector<std::string>	&Server::getNames(void) const { return this->_names; }
+int								Server::getClientMaxBodySize(void) const { return this->_client_max_body_size; }
+const std::string				&Server::getRoot(void) const { return this->_root; }
+const std::vector<std::string>	&Server::getIndexes(void) const { return this->_indexes; }
+const std::string				&Server::getCgiPass(void) const { return this->_cgi_pass; }
+//const std::string	&Server::getCgiExtension(void) const { return this->_cgi_extension; }
+//const std::string	&Server::getCgiPath(void) const { return this->_cgi_path; }
+
+// Setters
+bool	Server::setPort(int port) throw(std::exception) {
+	if (port < 0 || 65535 < port) {
+		throw (Server::InvalidPortException());
+	}
+	this->_port = port;
+	return (true);
+}
+
+bool	Server::setClientMaxBodySize(int client_max_body_size) throw(std::exception) {
+	if (client_max_body_size < 0) {
+		throw (Server::InvalidClientMaxBodySizeException());
+	}
+	this->_client_max_body_size = client_max_body_size;
+	return (true);
+}
+
+bool	Server::setRoot(const std::string &root) {
+	this->_root = root;
+	return (true);
+}
+
+bool	Server::setCgiPass(const std::string &cgi_pass) {
+	this->_cgi_pass = cgi_pass;
+	return (true);
+}
+
+//bool	Server::setCgiExtension(const std::string &cgi_extension) {
+//	this->_cgi_extension = cgi_extension;
+//	return (true);
+//}
+
+//bool	Server::setCgiPath(const std::string &cgi_path) {
+//	this->_cgi_path = cgi_path;
+//	return (true);
+//}
+
+// Utils
+bool	Server::findName(const std::string &name) const {
+	return (std::find(this->_names.begin(), this->_names.end(), name) != this->_names.end());
+}
+
+bool	Server::pushName(const std::string &name) {
+	if (this->findName(name)) {
+		return (true);
+	}
+	this->_names.push_back(name);
+	return (true);
+}
+
+bool	Server::findIndex(const std::string &index) const {
+	return (std::find(this->_indexes.begin(), this->_indexes.end(), index) != this->_indexes.end());
+}
+
+bool	Server::pushIndex(const std::string &index) {
+	this->_indexes.push_back(index);
+	return (true);
+}
+
+// Exception
+const char *Server::InvalidPortException::what() const throw() { return ("Invalid port"); }
+const char *Server::InvalidClientMaxBodySizeException::what() const throw() { return ("Invalid client_max_body_size"); }
+
+std::ostream	&operator<<(std::ostream &os, const Server &rhs) {
+	os << "Port: " << rhs.getPort() << '\n';
+	os << "Names: ";
+	for (std::vector<std::string>::const_iterator it = rhs.getNames().begin(); it != rhs.getNames().end(); it++) {
+		os << *it << ' ';
+	}
+	os << '\n';
+	os << "ClientMaxBodySize: " << rhs.getClientMaxBodySize() << '\n';
+	os << "Root: " << rhs.getRoot() << '\n';
+	os << "Indexed: ";
+	for (std::vector<std::string>::const_iterator it = rhs.getIndexes().begin(); it != rhs.getIndexes().end(); it++) {
+		os << *it << ' ';
+	}
+	os << '\n';
+	os << "CgiPass: " << rhs.getCgiPass() << '\n';
+	//os << "CgiExtension: " << rhs.getCgiExtension() << '\n';
+	//os << "CgiPath: " << rhs.getCgiPath() << '\n';
+	return (os);
+}
