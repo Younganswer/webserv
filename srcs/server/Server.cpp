b4ee998cfa1f2d07a75c073c4bcc998b46bf9ed6
Younganswer <dudtj1999@gmail.com>
2023-05-01 14:24:00 +0900

    [feat] Server initialize with Socket and Kqueue #23

--- srcs/server/Server.cpp
+++ srcs/server/Server.cpp
@@ -1,24 +1,42 @@
 #include "../../incs/server/Server.hpp"
 #include "../../incs/config/Config.hpp"
 
-Server::Server(const Config::map &config_map):
+Server::Server(const Config::map &config_map) throw(std::exception):
 	_port(std::atoi(config_map.find(Config::PORT)->second[0].c_str())),
 	_names(config_map.find(Config::SERVER_NAME)->second),
 	_root(config_map.find(Config::ROOT)->second[0]),
 	_indexes(config_map.find(Config::INDEX)->second),
-	_default_err_page(config_map.find(Config::ERROR_PAGE)->second[0]),
+	_default_error_page(config_map.find(Config::ERROR_PAGE)->second[0]),
 	_client_max_body_size(std::atoi(config_map.find(Config::CLIENT_MAX_BODY_SIZE)->second[0].c_str())),
-	_cgi_pass(config_map.find(Config::CGI_PASS)->second[0])
+	_cgi_pass(config_map.find(Config::CGI_PASS)->second[0]),
+	_socket(Socket()),
+	_kqueue(Kqueue())
 {
-	if (_port < 0 || 65535 < _port)
-		throw Server::InvalidPortException();
-	if (_client_max_body_size < 0)
-		throw Server::InvalidClientMaxBodySizeException();
+	if (Server::_is_valid_port(_port) == false) {
+		throw (Server::InvalidPortException());
+	}
+	if (Server::_is_valid_client_max_body_size(_client_max_body_size) == false) {
+		throw (Server::InvalidClientMaxBodySizeException());
+	}
+	try {
+		this->_socket = Socket(this->_port);
+		this->_kqueue = Kqueue(this->_socket.getFd());
+	} catch (const Socket::SocketCreationErrorException &e) {
+		throw (Socket::SocketCreationErrorException());
+	} catch (const Kqueue::KqueueCreationErrorException &e) {
+		throw (Kqueue::KqueueCreationErrorException());
+	} catch (...) {
+		throw (Server::UnknownErrorException());
+	}
 }
 Server::~Server(void) {}
 
+// Static
+bool	Server::_is_valid_port(int port) { return (0 <= port && port <= 65535); }
+bool	Server::_is_valid_client_max_body_size(int client_max_body_size) { return (0 <= client_max_body_size); }
+
 // Getters
-const std::string				&Server::getDefaultErrPage(void) const { return (this->_default_err_page); }
+const std::string				&Server::getDefaultErrorPage(void) const { return (this->_default_error_page); }
 int								Server::getPort(void) const { return (this->_port); }
 const std::vector<std::string>	&Server::getNames(void) const { return (this->_names); }
 int								Server::getClientMaxBodySize(void) const { return (this->_client_max_body_size); }
@@ -27,13 +45,19 @@ const std::vector<std::string>	&Server::getIndexes(void) const { return (this->_
 const std::string				&Server::getCgiPass(void) const { return (this->_cgi_pass); }
 
 // Util
-bool	init(void) {
-	
+bool	Server::init(void) throw(std::exception) {
+	try {
+		this->_socket.bind();
+		this->_socket.listen();
+	} catch (const Socket::SocketBindErrorException &e) {
+		throw (Socket::SocketBindErrorException());
+	} catch (const Socket::SocketListenErrorException &e) {
+		throw (Socket::SocketListenErrorException());
+	} catch (...) {
+		throw (Server::UnknownErrorException());
+	}
+	return (true);
 }
-
-// Exception
-const char	*Server::InvalidPortException::what(void) const throw() { return ("Invalid port"); }
-const char	*Server::InvalidClientMaxBodySizeException::what(void) const throw() { return ("Invalid client_max_body_size"); }
 
 std::ostream	&operator<<(std::ostream &os, const Server &rhs) {
 	os << "port: " << rhs.getPort() << '\n';
@@ -52,3 +76,8 @@ std::ostream	&operator<<(std::ostream &os, const Server &rhs) {
 	os << "cgi_pass: " << rhs.getCgiPass();
 	return (os);
 }
+
+// Exception
+const char	*Server::InvalidPortException::what(void) const throw() { return ("Invalid port"); }
+const char	*Server::InvalidClientMaxBodySizeException::what(void) const throw() { return ("Invalid client_max_body_size"); }
+const char	*Server::UnknownErrorException::what(void) const throw() { return ("Unknown error"); }
