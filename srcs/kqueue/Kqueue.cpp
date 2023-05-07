63f3004b5e980194f29ece95327b44ddd6b42c8f
Younganswer <dudtj1999@gmail.com>
2023-05-07 14:47:25 +0900

    [fix] Separate initializer from constructor
    * File descriptor is closed when destructor called
    * Desturctor called repeatedly when after calling copy constructor or copy assign operator
    * It is because of "Temporary Object"

--- srcs/kqueue/Kqueue.cpp
+++ srcs/kqueue/Kqueue.cpp
@@ -6,20 +6,12 @@
 #include <errno.h>
 
 // Constructor & Destructor
-Kqueue::Kqueue(void) {}
-Kqueue::Kqueue(int server_fd) {
-	if ((this->_kq_fd = kqueue()) == -1) {
-		throw (Kqueue::FailToCreateException());
-	}
-	try {
-		this->addEvent(server_fd);
-	} catch (const Kqueue::FailToControlException &e) {
-		close(this->_kq_fd);
-		throw (e);
-	}
+Kqueue::Kqueue(void): _fd(0) {
+	memset(this->_ev_set, 0, 2 * sizeof(struct kevent));
+	memset(this->_ev_list, 0, MAX_EVENTS * sizeof(struct kevent));
 }
-Kqueue::~Kqueue(void) { close(this->_kq_fd); }
-Kqueue::Kqueue(const Kqueue &ref): _kq_fd(ref._kq_fd) {
+Kqueue::~Kqueue(void) { if (this->_fd) close(this->_fd); }
+Kqueue::Kqueue(const Kqueue &ref): _fd(ref._fd) {
 	for (int i=0; i<2; i++) {
 		this->_ev_set[i] = ref._ev_set[i];
 	}
@@ -35,55 +27,55 @@ Kqueue	&Kqueue::operator=(const Kqueue &rhs) {
 	return (*this);
 }
 
-// Setters
-bool	Kqueue::setEvent(int fd, int filter, int flags, void *udata) throw(std::exception) {
-	if (filter == EVFILT_READ) {
-		EV_SET(&this->_ev_set[0], fd, filter, flags, 0, 0, udata);
-	} else if (filter == EVFILT_WRITE) {
-		EV_SET(&this->_ev_set[1], fd, filter, flags, 0, 0, udata);
-	} else {
-		throw (Kqueue::InvalidParameterException());
+// Util
+bool	Kqueue::init(int server_fd) throw(std::exception) {
+	if ((this->_fd = kqueue()) == -1) {
+		throw (Kqueue::FailToCreateException());
+	}
+	try {
+		this->addEvent(server_fd);
+	} catch (const Kqueue::FailToControlException &e) {
+		throw (e);
 	}
 	return (true);
 }
 
-// Util
-int	Kqueue::length(void) {
-	// TODO: Error occurs here
-	return (kevent(this->_kq_fd, NULL, 0, this->_ev_list, MAX_EVENTS, NULL));
+int		Kqueue::length(void) {
+	int	ret = kevent(this->_fd, NULL, 0, this->_ev_list, MAX_EVENTS, NULL);
+	
+	if (ret == -1) {
+		throw (Kqueue::FailToGetEventException());
+	}
+	return (ret);
 }
 
-int	Kqueue::getEventFd(int idx) const throw(std::exception) {
-	if (idx < 0 || MAX_EVENTS <= idx) {
-		throw (Kqueue::OutOfRangeException());
-	}
+int		Kqueue::getEventFd(int idx) const throw(std::exception) {
 	return (this->_ev_list[idx].ident);
 }
 
-bool	Kqueue::addEvent(int client_fd) {
-	this->setEvent(client_fd, EVFILT_READ, EV_ADD | EV_ENABLE, NULL);
-	this->setEvent(client_fd, EVFILT_WRITE, EV_ADD | EV_ENABLE, NULL);
-	if (kevent(this->_kq_fd, this->_ev_set, 2, NULL, 0, NULL) == -1) {
-		close(client_fd);
+bool	Kqueue::addEvent(int socket_fd) {
+	EV_SET(&this->_ev_set[0], socket_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+	EV_SET(&this->_ev_set[1], socket_fd, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, NULL);
+	if (kevent(this->_fd, this->_ev_set, 2, NULL, 0, NULL) == -1) {
+		close(socket_fd);
 		throw (Kqueue::FailToControlException());
 	}
 	return (true);
 }
 
-bool	Kqueue::deleteEvent(int client_fd) {
-	close(client_fd);
-	this->setEvent(client_fd, EVFILT_READ, EV_DELETE, NULL);
-	this->setEvent(client_fd, EVFILT_WRITE, EV_DELETE, NULL);
-	if (kevent(this->_kq_fd, this->_ev_set, 2, NULL, 0, NULL) == -1) {
+bool	Kqueue::deleteEvent(int socket_fd) {
+	EV_SET(&this->_ev_set[0], socket_fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+	EV_SET(&this->_ev_set[1], socket_fd, EVFILT_WRITE, EV_DELETE, 0, 0, NULL);
+	if (kevent(this->_fd, this->_ev_set, 2, NULL, 0, NULL) == -1) {
+		close(socket_fd);
 		throw (Kqueue::FailToControlException());
 	}
+	close(socket_fd);
 	return (true);
 }
 
 // Exception
 const char	*Kqueue::FailToCreateException::what(void) const throw() { return ("Kqueue: Fail to create"); }
 const char	*Kqueue::FailToControlException::what(void) const throw() { return ("Kqueue: Fail to control"); }
-const char	*Kqueue::InvalidParameterException::what(void) const throw() { return ("Kqueue: Invalid parameter"); }
 const char	*Kqueue::FailToGetEventException::what(void) const throw() { return ("Kqueue: Fail to get event"); }
 const char	*Kqueue::TimeoutException::what(void) const throw() { return ("Kqueue: Timeout"); }
-const char	*Kqueue::OutOfRangeException::what(void) const throw() { return ("Kqueue: Out of range"); }
--- srcs/socket/Socket.cpp
+++ srcs/socket/Socket.cpp
@@ -6,29 +6,8 @@
 #include <memory.h>
 
 // Constructor & Destructor
-Socket::Socket(void) {}
-Socket::Socket(int port) {
-	if ((this->_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		throw (Socket::FailToCreateException());
-	}
-	this->_enable = 1;
-	setsockopt(this->_fd, SOL_SOCKET, SO_REUSEADDR, &this->_enable, sizeof(this->_enable));
-	memset(&this->_addr, 0, sizeof(this->_addr));
-	this->_addr.sin_family = AF_INET;
-	this->_addr.sin_port = htons(port);
-	this->_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-	try {
-		this->bind();
-		this->listen();
-	} catch (const Socket::FailToBindException &e) {
-		close(this->_fd);
-		throw (e);
-	} catch (const Socket::FailToListenException &e) {
-		close(this->_fd);
-		throw (e);
-	}
-}
-Socket::~Socket(void) {}
+Socket::Socket(void): _fd(0), _enable(0) { memset(&this->_addr, 0, sizeof(struct sockaddr_in)); }
+Socket::~Socket(void) { if (this->_fd) close(this->_fd); }
 Socket::Socket(const Socket &ref): _fd(ref._fd), _addr(ref._addr) {}
 Socket	&Socket::operator=(const Socket &rhs) {
 	if (this != &rhs) {
@@ -42,20 +21,6 @@ Socket	&Socket::operator=(const Socket &rhs) {
 int		Socket::getFd(void) const { return (this->_fd); }
 
 // Utils
-bool	Socket::bind(void) throw(std::exception) {
-	if (::bind(this->_fd, (struct sockaddr *) &this->_addr, sizeof(this->_addr)) == -1) {
-		throw (Socket::FailToBindException());
-	}
-	return (true);
-}
-
-bool	Socket::listen(void) throw(std::exception) {
-	if (::listen(this->_fd, 10) == -1) {
-		throw (Socket::FailToListenException());
-	}
-	return (true);
-}
-
 int	Socket::accept(void) throw(std::exception) {
 	int					client_fd;
 	struct sockaddr_in	client_addr;
@@ -75,6 +40,30 @@ int	Socket::accept(void) throw(std::exception) {
 		throw (Socket::FailToControlException());
 	}
 	return (client_fd);
+}
+
+bool	Socket::init(int port) throw(std::exception) {
+	// Create socket
+	if ((this->_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		throw (Socket::FailToCreateException());
+	}
+	
+	// Set socket options
+	this->_enable = 1;
+	setsockopt(this->_fd, SOL_SOCKET, SO_REUSEADDR, &this->_enable, sizeof(this->_enable));
+	memset(&this->_addr, 0, sizeof(this->_addr));
+	this->_addr.sin_family = AF_INET;
+	this->_addr.sin_port = htons(port);
+	this->_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+	// Bind & Listen
+	if (bind(this->_fd, (struct sockaddr *)&this->_addr, sizeof(this->_addr)) == -1) {
+		throw (Socket::FailToBindException());
+	}
+	if (listen(this->_fd, 10) == -1) {
+		throw (Socket::FailToListenException());
+	}
+	return (true);
 }
 
 // Exception
