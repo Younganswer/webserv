f3fbbd31fd9a19b87a1927d2dddaaae5ce672678
Younganswer <dudtj1999@gmail.com>
2023-05-07 14:44:40 +0900

    [fix] Separate initializer from constructor
    * File descriptor is closed when destructor called
    * Desturctor called repeatedly when after calling copy constructor or copy assign operator
    * It is because of "Temporary Object"

--- incs/kqueue/Kqueue.hpp
+++ incs/kqueue/Kqueue.hpp
@@ -9,23 +9,20 @@ class Kqueue {
 		static const int	MAX_EVENTS = 16;
 
 	private:
-		int				_kq_fd;
+		int				_fd;
 		struct kevent	_ev_set[2];
 		struct kevent	_ev_list[MAX_EVENTS];
-
-		// Setters
-		bool	setEvent(int fd, int filter, int flags, void *udata) throw(std::exception);
 
 	// Constructor & Destructor
 	public:
 		Kqueue(void);
-		Kqueue(int server_fd);
 		~Kqueue(void);
 		Kqueue(const Kqueue &ref);
 		Kqueue	&operator=(const Kqueue &rhs);
 
 	// Util
 	public:
+		bool	init(int server_fd) throw(std::exception);
 		int		length(void);
 		bool	addEvent(int client_fd);
 		bool	deleteEvent(int client_fd);
@@ -43,22 +40,12 @@ class Kqueue {
 				virtual const char *what() const throw();
 		};
 
-		class InvalidParameterException: public std::exception {
-			public:
-				virtual const char *what() const throw();
-		};
-
 		class FailToGetEventException: public std::exception {
 			public:
 				virtual const char *what() const throw();
 		};
 
 		class TimeoutException: public std::exception {
-			public:
-				virtual const char *what() const throw();
-		};
-
-		class OutOfRangeException: public std::exception {
 			public:
 				virtual const char *what() const throw();
 		};
