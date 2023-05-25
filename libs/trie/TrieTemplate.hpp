c9566fc4d70f17473f015ecf6767ebd377d05fb9
daekuelee <daekuelee@gmail.com>
2023-05-26 08:23:00 +0900

    trie expand Template ~ing

--- libs/trie/TrieTemplate.hpp
+++ libs/trie/TrieTemplate.hpp
@@ -2,11 +2,10 @@
 # define TRITEMPLATE_HPP
 namespace ft {
 
-
 template <typename T, typename Value>
 struct TrieNode {
     bool isEndOfWord;
-    std::map<T, ft::unique_ptr<TrieNode<T, Value>>> children;
+    std::map<T, ft::unique_ptr<TrieNode<T, Value> > > children;
     ft::unique_ptr<Value> value;  // Store the actual data here.
 
     TrieNode() : isEndOfWord(false) {}
@@ -15,30 +14,32 @@ struct TrieNode {
 template <typename T, typename Value>
 class Trie {
 private:
-    ft::unique_ptr<TrieNode<T, Value>> root;
+    ft::unique_ptr<TrieNode<T, Value> > root;
 
 public:
     Trie() : root(new TrieNode<T, Value>()) {}
 
-    void insert(const std::basic_string<T> &key, const Value &value) {
+    void insert(const T* key, size_t length, const Value& value) {
         TrieNode<T, Value>* current = root.get();
-        for (int i = 0; i < key.length(); ++i) {
+        for (size_t i = 0; i < length; ++i) {
             T c = key[i];
-            if (current->children.find(c) == current->children.end())
+            typename std::map<T, ft::unique_ptr<TrieNode<T, Value> > >::iterator it = current->children.find(c);
+            if (it == current->children.end())
                 current->children[c].reset(new TrieNode<T, Value>());
             current = current->children[c].get();
         }
         current->isEndOfWord = true;
-        current->value.reset(new Value(value)); 
+        current->value.reset(new Value(value));
     }
 
-    Value* search(const std::basic_string<T> &key) {
+    Value* search(const T* key, size_t length) {
         TrieNode<T, Value>* current = root.get();
-        for (int i = 0; i < key.length(); ++i) {
+        for (size_t i = 0; i < length; ++i) {
             T c = key[i];
-            if (current->children.find(c) == current->children.end())
+            typename std::map<T, ft::unique_ptr<TrieNode<T, Value> > >::iterator it = current->children.find(c);
+            if (it == current->children.end())
                 return NULL;
-            current = current->children[c].get();
+            current = it->second.get();
         }
         return current != NULL && current->isEndOfWord ? current->value.get() : NULL;
     }
