0db7f9a8e83e0a710e5a37af99bbaba87466c70b
Younganswer <dudtj1999@gmail.com>
2023-05-26 18:09:43 +0900

    [Feat] Feature Trie Class #51

---
+++ libs/trie/TestTrie.cpp
@@ -0,0 +1,23 @@
+#include "Trie.hpp"
+#include <iostream>
+
+int	main(void) {
+	ft::Trie<int>	trie;
+
+	trie.insert("/", 0);
+	trie.insert("/abc", 1);
+	trie.insert("/abc/de", 2);
+	trie.insert("/abc/def", 3);
+
+	try {
+		std::cout << trie.getData("/index.html") << '\n';
+		std::cout << trie.getData("/ab/index.html") << '\n';
+		std::cout << trie.getData("/abc/d/index.html") << '\n';
+		std::cout << trie.getData("/abc/de/index.html") << '\n';
+		std::cout << trie.getData("/abc/def/index.html") << '\n';
+		std::cout << trie.getData("/abc/deg/index.html") << '\n';
+	} catch (const std::exception &e) {
+		std::cout << "Error: " << e.what() << '\n';
+	}
+	return (0);
+}
