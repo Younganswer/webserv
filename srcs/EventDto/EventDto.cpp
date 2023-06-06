b55238b38c419f0b3a1e68aa5289e2dc63bf3d83
daekuelee <daekuelee@gmail.com>
2023-06-06 21:58:40 +0900

    fix compile

--- srcs/EventDto/EventDto.cpp
+++ srcs/EventDto/EventDto.cpp
@@ -2,5 +2,6 @@
 
 EventDto::EventDto(int fd, const PhysicalServer::VirtualServerMap *TargetMap) : _fd(fd), _TargetMap(TargetMap) {}
 EventDto::EventDto(int fd)  : _fd(fd), _TargetMap(NULL) {}
+EventDto::~EventDto(void) {}
 int EventDto::getFd() const { return _fd; }
 const PhysicalServer::VirtualServerMap *EventDto::getTargetMap() const { return _TargetMap; }
