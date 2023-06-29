b62807cb705cb7c6de86f8dc8d234d38dd7a5e2a
Younganswer <dudtj1999@gmail.com>
2023-06-29 18:42:04 +0900

    [Rename] #103

--- srcs/http/parser/FileNameGenerator.cpp
+++ srcs/http/parser/FileNameGenerator.cpp
@@ -2,25 +2,25 @@
 
 bool FileNameGenerator::isFileExists(const std::string& filename)
 {
-    std::ifstream file(filename.c_str());
-    return file.good();
+	std::ifstream file(filename.c_str());
+	return file.good();
 }
 
 std::string FileNameGenerator::generateUniqueFileName()
 {
-    std::stringstream filenameStream;
-    
-    std::time_t currentTime = std::time(NULL);
-    filenameStream << "file_" << currentTime << ".txt";
-    
-    std::string filename = filenameStream.str();
-    while (isFileExists(FILE_LOCATION + filename))
-    {
-        currentTime++;
-        filenameStream.str("");
-        filenameStream << "file_" << currentTime << ".txt";
-        filename = filenameStream.str();
-    }
-    
-    return FILE_LOCATION + filename;
+	std::stringstream filenameStream;
+	
+	std::time_t currentTime = std::time(NULL);
+	filenameStream << "file_" << currentTime << ".txt";
+	
+	std::string filename = filenameStream.str();
+	while (isFileExists(filename))
+	{
+		currentTime++;
+		filenameStream.str("");
+		filenameStream << "file_" << currentTime << ".txt";
+		filename = filenameStream.str();
+	}
+	
+	return FILE_LOCATION + filename;
 }
--- srcs/http/test/MultipartBodyRequestTest.cpp
+++ srcs/http/test/MultipartBodyRequestTest.cpp
@@ -1,42 +1,47 @@
 #include "../../../incs/Http/Parser/HttpRequestParser.hpp"
 
+std::string createMultipartFormData(const std::string& boundary, const std::string& fieldName, const std::string& fieldValue, const std::string& fileName, const std::string& fileContent) {
+	std::string formData;
+	formData += "--" + boundary + "\r\n";
+	formData += "Content-Disposition: form-data; name=\"" + fieldName + "\"\r\n";
+	formData += "\r\n";
+	formData += fieldValue + "\r\n";
+
+	formData += "--" + boundary + "\r\n";
+	formData += "Content-Disposition: form-data; name=\"" + fieldName + "\"; filename=\"" + fileName + "\"\r\n";
+	formData += "Content-Type: application/octet-stream\r\n";
+	formData += "\r\n";
+	formData += fileContent + "\r\n";
+
+	formData += "--" + boundary + "--\r\n";
+
+	return formData;
+}
+
 std::string createMultipartRequestBody(const std::string& fieldName, const std::string& fieldValue, const std::string& fileName, const std::string& fileContent) {
-    std::string requestBody;
-    std::string boundary = "boundary";
+	std::string requestBody;
+	std::string boundary = "----Boundary" + std::to_string(std::rand());
+	requestBody += "POST / Http/1.1\r\n";
+	requestBody += "Host: localhost:8080\r\n";
+	requestBody += "User-Agent: curl/7.64.1\r\n";
+	requestBody += "Content-Type: multipart/form-data; boundary=" + boundary + "\r\n";
+	requestBody += "Content-Length: " + std::to_string(createMultipartFormData(boundary, fieldName, fieldValue, fileName, fileContent).length()) + "\r\n";
+	requestBody += "\r\n";
+	requestBody += createMultipartFormData(boundary, fieldName, fieldValue, fileName, fileContent);
 
-    requestBody += "--" + boundary + "\r\n";
-    requestBody += "Content-Disposition: form-data; name=\"" + fieldName + "\"; filename=\"" + fileName + "5" + "\"\r\n";
-    requestBody += "\r\n";
-    requestBody += fieldValue + "\r\n";
-
-    requestBody += "--" + boundary + "\r\n";
-    requestBody += "Content-Disposition: form-data; name=\"" + fieldName + "\"; filename=\"" + fileName + "6" + "\"\r\n";
-    requestBody += "\r\n";
-    requestBody += fileContent + "\r\n";
-
-    requestBody += "--" + boundary + "--\r\n";
-
-    return requestBody;
+	return requestBody;
 }
 
 int main(){
-	std::string fieldName = "field1";
-    std::string fieldValue = "value1lskdajlksajdflkjsdaf";
-    std::string fileName = "example1.txt";
-    std::string fileContent = "value2psadouf;asdjflsadkjhflksdajflks";
-
-    std::string multipartBodyRequest = createMultipartRequestBody(fieldName, fieldValue, fileName, fileContent);
-
-    std::string requestHeaders = "POST /test.html HTTP/1.1\r\n"
-                                 "Host: example.org\r\n"
-                                 "Content-Type: multipart/form-data; boundary=\"boundary\"\r\n"
-                                 "Content-Length: " + std::to_string(multipartBodyRequest.length()) + "\r\n"
-                                 "\r\n";
-
-    std::string completeRequest = requestHeaders + multipartBodyRequest;
-
+	std::string fieldName = "text_field";
+	std::string fieldValue = "This is a text field value.";
+	std::string fileName = "example.txt";
+	std::string fileContent = "This is the content of the file.";
+	
+	 std::string multipartBodyRequest = createMultipartRequestBody(fieldName, fieldValue, fileName, fileContent);
+	
 	std::vector<char> httpReqeustBuffer;
-	httpReqeustBuffer.assign(completeRequest.begin(), completeRequest.end());
+	httpReqeustBuffer.assign(multipartBodyRequest.begin(), multipartBodyRequest.end());
 	HttpRequestParser httpRequestParser;
 	httpRequestParser.parseRequest(httpReqeustBuffer, 1000000);
 	ft::shared_ptr <HttpRequest> httpRequest = httpRequestParser.getHttpRequest();
@@ -48,19 +53,19 @@ int main(){
 	std::cout << httpRequest->isBodyLong() << std::endl;
 	std::vector<MultipartRequest> multiparts = httpRequest->getMultipartRequests();
 
-    std::cout << "Multipart requests: " << multiparts.size() << std::endl;
-    for(size_t i = 0; i < multiparts.size(); i++){
-        std::cout << "part " << i << std::endl;
-        if (httpRequest->isBodyLong())
-            std::cout << multiparts[i].getBodyDataFilename() << std::endl;
-        else
-            std::cout << multiparts[i].getMemoryBody().data() << std::endl;
-        std::multimap<std::string, std::string> _headers = multiparts[i].getHeaders();
-        std::multimap<std::string, std::string>::iterator it;
-        for (it = _headers.begin(); it != _headers.end(); it++)
-        {
-            std::cout << it->first << " : " << it->second << std::endl;
-        }
-    }
+	std::cout << "Multipart requests: " << multiparts.size() << std::endl;
+	for(size_t i = 0; i < multiparts.size(); i++){
+		std::cout << "part " << i << std::endl;
+		if (httpRequest->isBodyLong())
+			std::cout << multiparts[i].getBodyDataFilename() << std::endl;
+		else
+			std::cout << multiparts[i].getMemoryBody().data() << std::endl;
+		std::multimap<std::string, std::string> _headers = multiparts[i].getHeaders();
+		std::multimap<std::string, std::string>::iterator it;
+		for (it = _headers.begin(); it != _headers.end(); it++)
+		{
+			std::cout << it->first << " : " << it->second << std::endl;
+		}
+	}
 
 }
