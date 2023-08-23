8533e48d66a18e3ec5ed71dabe01aed9374d6262
daekuelee <daekuelee@gmail.com>
2023-08-23 17:23:49 +0900

    fixing buffer base

--- libs/Library/Test/HashTable.cpp
+++ libs/Library/Test/HashTable.cpp
@@ -9,15 +9,32 @@ class a{
 class b{
 
 };
-int main(void) {
-	// ft::HashTable<a, int> x;
-	// ft::HashTable<std::string,int> x;
-	// ft::HashTable<b, int> x;
+
+void foo(){
 	ft::HashTable<std::string, int> a;
 	a.insert("1", 3);
 	a.insert("13", 7);
 	std::cout << a.get("1").value_or(-1) << '\n';
 	std::cout << a.get("13").value_or(-1) << '\n';
 	std::cout << a.get("14").value_or(-1) <<'\n';
-	std::cout << ft::type::is_std_string<std::string>::value << '\n';
+	for (ft::HashTable<std::string,int>::iterator it = a.begin(); it != a.end(); ++it){
+		std::cout << (*it)->second << '\n';
+	}
+	std::cout << "Int \n";
+	ft::HashTable<int, int > b;
+	b.insert(1, 3);
+	b.insert(4, 7);
+	std::cout << b.get(1).value_or(-1) << '\n';
+	std::cout << b.get(4).value_or(-1) << '\n';
+	std::cout << b.get(7).value_or(-1) <<'\n';
+	for (ft::HashTable<int,int>::iterator it = b.begin(); it != b.end(); ++it){
+		std::cout << (*it)->second << '\n';
+	}	// std::cout << ft::type::is_std_string<std::string>::value << '\n';
+}
+int main(void) {
+	// ft::HashTable<a, int> x;
+	// ft::HashTable<std::string,int> x;
+	ft::HashTable<b, int> x;
+	// foo();
+	system("leaks a.out");
 }
