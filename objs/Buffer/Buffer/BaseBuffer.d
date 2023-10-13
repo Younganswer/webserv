4d713a16e551d5822762e458ddb26083002c4854
daekuelee <daekuelee@gmail.com>
2023-10-13 10:53:36 +0900

    saved

--- objs/Buffer/Buffer/BaseBuffer.d
+++
@@ -1,9 +0,0 @@
-objs/Buffer/Buffer/BaseBuffer.o: srcs/Buffer/Buffer/BaseBuffer.cpp \
-  incs/Buffer/Buffer/BaseBuffer.hpp \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/cstddef \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__config \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__config_site \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/pthread.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/version \
-  /Library/Developer/CommandLineTools/usr/lib/clang/14.0.0/include/stddef.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__nullptr
